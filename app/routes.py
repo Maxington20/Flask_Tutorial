9722c823823acba6abbb6d84258d09814dce0af5
Max Herrington <max.herrington@gmail.com>
2019-05-20 19:42:06 -0400

    db stuff

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -29,6 +29,6 @@ def login():
             form.username.data, form.remember_me.data
         ))
 
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
     
